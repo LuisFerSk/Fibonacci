69b03c5e4867692b6096a580c38f2f4913199ca9
LuisFerSk <lcampomontero@gmail.com>
2022-06-10 06:23:32 -0500

    Javascript Diseño de prueba - Debe empezar con los valores 0 y 1

--- Javascript/test/fibonacci.test.js
+++ Javascript/test/fibonacci.test.js
@@ -1,11 +1,16 @@
 const CalcularSerieFibonacci = require('../index');
 
-// La serie de fibonacci solo puede tener números naturales
-test('Solo debe tener números naturales', () => {
-    let serieFibonacci = CalcularSerieFibonacci();
+let serieFibonacci = CalcularSerieFibonacci();
 
+// 1. La serie de fibonacci solo puede tener números naturales
+test('Solo debe tener números naturales', () => {
     const result = serieFibonacci.find((numero) => numero % 1 !== 0) ? false : true;
 
     expect(result).toEqual(true);
 })
 
+// 2. La sere de fibonacci empieza con los valores 0 y 1
+test('Debe empezar con los valores 0 y 1', () => {
+    const result = serieFibonacci.slice(0, 2);
+    expect(result).toEqual([0, 1]);
+})
