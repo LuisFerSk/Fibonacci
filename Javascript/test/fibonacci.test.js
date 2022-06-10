437f21ee87f0c8641bf332c2a247c4214e8e93c3
LuisFerSk <lcampomontero@gmail.com>
2022-06-10 06:37:44 -0500

    Javascript Diseño de prueba - Cada termino desde la pocision 3 es suma de los dos anteriores

--- Javascript/test/fibonacci.test.js
+++ Javascript/test/fibonacci.test.js
@@ -9,8 +9,16 @@ test('Solo debe tener números naturales', () => {
     expect(result).toEqual(true);
 })
 
-// 2. La sere de fibonacci empieza con los valores 0 y 1
+// 2. La serie de fibonacci empieza con los valores 0 y 1
 test('Debe empezar con los valores 0 y 1', () => {
     const result = serieFibonacci.slice(0, 2);
     expect(result).toEqual([0, 1]);
+})
+
+// 3. En la serie de fibonacci, desde la posición 3, cada termino es la suma de los dos anteriores
+test('Cada termino desde la pocision 3 es suma de los dos anteriores', () => { 
+    for(let i = 3; i < serieFibonacci.length; i++) {
+        const result = serieFibonacci[i] === serieFibonacci[i - 1] + serieFibonacci[i - 2];
+        expect(result).toEqual(true);
+    }
 })
